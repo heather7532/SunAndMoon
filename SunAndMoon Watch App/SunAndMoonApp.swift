4766c8813c482c5c6b3a82418dbd60ae894edbfa
Heather <heather.e.gulledge@gmail.com>
2025-07-28 12:55:47 -0500

    Initial Commit

---
+++ SunAndMoon Watch App/SunAndMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunAndMoonApp.swift
+//  SunAndMoon Watch App
+//
+//  Created by Heather Gulledge on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SunAndMoon_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
